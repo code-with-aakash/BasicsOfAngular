9b89427ebf8d82006fb9a537ed00bc36da9e4800
Aakash Yadav <aakashy919@gmail.com>
2022-09-05 19:40:07 +0530

    ng if directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,8 @@ import { Component } from '@angular/core';
   
 })
 export class AppComponent {
- tweet = {
-  body:'nothing',
-  isLiked: true,
-  likesCount: 10
-  
- }
+
+  courses =[1];
   
   
 }
